5e3195df00fca2cb72a7ed164a96a4e03714a5ad
sshreyas267 <sshreyas267@gmail.com>
2022-02-27 15:31:03 +0530

    Update FAQ.jsx

--- client/src/components/userpage/FAQ.jsx
+++ client/src/components/userpage/FAQ.jsx
@@ -13,13 +13,13 @@ import { CheckIcon } from '@chakra-ui/icons';
 
 
 const features = [{id: 1, title:"Who are we?" , text:"We are Team Gryffindor, a group of students based out of Chennai"},
-{id: 2, title:"What is this about?" , text:"This is a place for all those who love to spot shipwrecks and want to add some of their own!"},
-{id: 3, title:"What are shipwrecks actually?" , text:"Well, A shipwreck is the wreckage of a ship that is located either beached on land or sunken to the bottom of a body of water. Shipwrecking may be intentional or unintentional."},
-{id: 4, title:"So what regions do you cover?" , text:"For now we cover the shipwrecks in the Asia-Pacific region with ideas of expanding throughout the globe!"},
-{id: 5, title:"How do I add my own shipwreck data?" , text:"After logging in to the Dashboard, you can fill up our wreck in form and add to our database."},
+{id: 2, title:"What is this about?" , text:"We have designed a portal for spotting and plotting disasters around the world with provision to add your own disaster site!"},
+{id: 3, title:"What disasters are we talking about here?" , text:"Disasters such as Tsunami, Storm, Floods in the Indian Subcontinent can be spotted and added so far."},
+{id: 4, title:"What is the Satellite Image identifier?" , text:"By providing a satellite image of the hurricane damage site, we can accurately provide information as to how much damage has been incurred. "},
+{id: 5, title:"How do I add my own disaster data?" , text:"After logging in to our Dashboard, you can fill up our disaster info form and add to our database."},
 {id: 6, title:"Does my data get added instantly?" , text:"Once we verify the authencity of your information, it will be published onto our database."},
-{id: 7, title:"How authentic and accurate is the shipwreck data?" , text:"When an user adds details of a shipwreck, our team verifies the authencity of the data by requesting the users to give necessary proofs suppoprting their data. The data is accurate upto 1km from the marked location."},
-{id: 8, title:"Customer Service" , text:"For any more queries, click on the chat icon below to talk directly to our representative."}
+{id: 7, title:"How authentic and accurate is the disaster data?" , text:"When an user adds details of a disaster site, our team verifies the authencity of the data by contacting the local officials around said location and we confirm the status. The data is accurate upto 1km from the marked location."},
+{id: 8, title:"Any Queries" , text:"For any more queries, click on the chat icon below to interact with our AI bot, Alan."}
 ]
 export default function FAQs() {
   return (
